73391ee5cb28030326d7c6d71d02fdb3b08b791f
jair <jairdeyner730791@gmail.com>
2022-04-12 21:23:57 -0500

    feat: Navabar and login design

---
+++ src/components/ui/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div className="navbar navbar-dark bg-dark mb-4">
+      <span className="navbar-brand">Jose</span>
+
+      <button className="btn btn-outline-danger">
+        <i className="fas fa-sign-out-alt"></i> <span>Salir</span>
+      </button>
+    </div>
+  );
+};
+export default Navbar;
